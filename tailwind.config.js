b053f7f44986524f97adce332fbe76e20d7c3960
Dmitry Conquer <dmitry.conquer@gmail.com>
2023-11-15 11:33:56 +0200

    update (adaptive)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,36 @@
 module.exports = {
   content: ["./src/scss/**/*.scss", "./src/**/*.html"],
-  theme: { extend: {} },
-  variants: { extend: {} },
-  plugins: [],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1200px",
+      "2xl": "1350px",
+    },
+    colors: {
+      primary: {
+        DEFAULT: "#2F58CD",
+        dark: "#2B3467",
+      },
+      gray: {
+        DEFAULT: "#414042",
+        100: "#F6F6F6",
+        200: "#BCB8BB",
+      },
+      danger: {
+        DEFAULT: "#E81313",
+      },
+      white: {
+        DEFAULT: "#FFF",
+      }
+    },
+    extend: {
+    
+    },
+  },
 };
